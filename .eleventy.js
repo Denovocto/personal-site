9725771c2fb2b6fe11809aefa1bc8f66ae9e0f39
denovocto <jejoshadez@gmail.com>
2023-03-01 18:02:01 -0400

    replace pug with nunjucks because there is better support for it

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,22 @@
+const postcss = require('postcss');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addNunjucksAsyncFilter('postcss', (cssCode, done) => {
+        postcss([tailwindcss(require('./tailwind.config.js')), autoprefixer()])
+          .process(cssCode)
+          .then(
+            (r) => done(null, r.css),
+            (e) => done(e, null)
+          );
+      });
+    eleventyConfig.addWatchTarget('src/css/**/*.css');
     return {
         dir: {
             input: 'src',
             output: 'public',
         },
-        templateFormats: ['html', 'md', 'pug'],
+        templateFormats: ['html', 'md', 'njk'],
     };
 };
