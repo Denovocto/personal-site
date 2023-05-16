46dd7ed8fe8adcc27edb60bbe8db68d6a1f8e9b1
denovocto <jejoshadez@gmail.com>
2023-05-16 19:12:41 -0400

    fix tailwindUI content configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{md,html,pug}"],
+  content: ["./src/**/*.{md,html,njk}"],
   theme: {
     extend: {},
   },
